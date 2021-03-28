a2b551df7392393f9b48c37f30ebd81e2417d958
unknown <apurvapatel184@gmail.com>
2021-03-28 17:34:39 +0530

    fourth

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget {
+class homePage extends StatefulWidget {
   @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget buildCafeNames() {
+    return Column(
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+              },
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
+                backgroundColor: MaterialStateProperty.all(Color(0xffE5EFFF)),
+                padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+              ),
+              child: Text(
+                'Ice Berg',
+                style: TextStyle(
+                    color: Color(0xff5961F9),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+        ),
+        Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+              },
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
+                backgroundColor: MaterialStateProperty.all(Color(0xffE5EFFF)),
+                padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+              ),
+              child: Text(
+                'Danis Cafe',
+                style: TextStyle(
+                    color: Color(0xff5961F9),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+        ),
+        Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+              },
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
+                backgroundColor: MaterialStateProperty.all(Color(0xffE5EFFF)),
+                padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+              ),
+              child: Text(
+                'Krishna Foods',
+                style: TextStyle(
+                    color: Color(0xff5961F9),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+        ),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      body: SizedBox(
+        width: 411,
+        height: 731,
+        child: Material(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 31,
+              right: 30,
+              top: 65,
+              bottom: 100,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Cafes",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 48,
+                  ),
+                ),
+                SizedBox(height: 20),
+                buildCafeNames(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
